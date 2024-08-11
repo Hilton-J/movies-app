da7ae86e9a0ff15affbac1ad0679eccfb1b6acfc
Hilton-J <lhmajola@outlook.com>
2024-08-11 04:18:23 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,59 @@ import HomePage from './pages/HomePage'
 import SeriesPage from './pages/SeriesPage'
 import MoviesPage from './pages/MoviesPage'
 import NotFoundPage from './pages/NotFoundPage'
+import AddPage from './pages/AddPage'
+import ViewPage from './pages/ViewPage'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/series' element={<SeriesPage />} />
-      <Route path='/movies' element={<MoviesPage />} />
 
-      <Route path='*' element={<NotFoundPage />} />
-
-    </Route>
-  )
-)
 
 const App = () => {
+  //ADD NEW JOB
+  // const addJob = async (newJob) => {
+  //   await fetch('/api/jobs', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(newJob),
+  //   });
+  //   return;
+  // };
+
+
+  //DELETE JOB 
+  // const deleteJob = async (id) => {
+  //   await fetch(`/api/jobs/${id}`, {
+  //     method: 'DELETE'
+  //   });
+  //   return;
+  // };
+
+  //UPDATE JOB
+  // const updateJob = async (job) => {
+  //   await fetch(`/api/jobs/${job.id}`, {
+  //     method: 'PUT',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(job),
+  //   });
+  //   return;
+  // };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/series' element={<SeriesPage />} />
+        <Route path='/movies' element={<MoviesPage />} />
+        <Route path='/view/:id' element={<ViewPage />} />
+        <Route path='/add/:id' element={<AddPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+
+      </Route>
+    )
+  )
+
   return <RouterProvider router={router} />
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,23 @@
-import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { NavLink, useLocation } from 'react-router-dom'
+import { useState } from 'react';
 
 const NavBar = () => {
   const linkClass = ({ isActive }) => isActive ? 'text-color hover:text-[#7379ff]' : 'hover:text-[#7379ff]';
+  const location = useLocation();
+
+
+
+  // if(location.pathname === '/series')
+
+  const header = location.pathname === '/series' ? 'LATEST SERIES' : location.pathname === '/movies' ? 'LATEST MOVIES' : '';
+
 
   return (
-    <header className='bg-white bg-bgimage bg-no-repeat bg-cover h-[23rem] bg-center'>
+    <header className='bg-white bg-bgimage bg-no-repeat bg-cover h-[23rem] bg-center flex flex-col'>
       <nav className='flex justify-center'>
-        <div className='text-white grid grid-cols-3 w-[90%] py-auto mt-16'>
-          <NavLink to={'/'} className=''><span className='text-[#7379ff]'>Enter-</span>Stream</NavLink>
+        <div className='text-white grid grid-cols-3 w-[90%] py-auto mt-9 ' >
+          <NavLink to={'/'} className='hover:text-[#7379ff]'><span className='text-[#7379ff] hover:text-white'>Enter-</span>Stream</NavLink>
 
           <div className='flex gap-10 justify-center'>
             <NavLink className={linkClass} to={'/movies'}>MOVIES</NavLink>
@@ -17,8 +27,15 @@ const NavBar = () => {
           <div className='flex justify-end'><NavLink to={'/subscribe'} className='bg-color py-1 px-9 rounded-3xl hover:bg-transparent hover:text-[#7379ff]'>SUBSCRIBE</NavLink></div>
         </div>
       </nav>
+      <div className='flex flex-1 justify-center items-center'>
+        <h1 className='text-white text-4xl'>{header}</h1>
+      </div>
     </header>
   )
-}
+};
+
+NavBar.propTypes = {
+  header: PropTypes.string
+};
 
 export default NavBar
