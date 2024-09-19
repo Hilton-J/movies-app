cb2af77665f077052ae5b490774f7dd0f27510c8
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-09-19 15:50:01 +0200

    Npm

--- src/pages/AddPage.jsx
+++ src/pages/AddPage.jsx
@@ -46,20 +46,10 @@ const AddItem = ({ addItemSubmit }) => {
     };
 
     await addItemSubmit(newItem, type);
-    // const url = `/api/${type}`
-    // await fetch(url, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify(newItem),
-    // });
-
-
+ 
     toast.success(`${type} ADDED successfully!`);
 
     return navigate(`/${type}`);
-
   };
 
   const handleGenreSelect = (selectedList) => {
